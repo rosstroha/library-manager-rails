339364f1a24386088454b0ccbc85790466bb71e2
Troha <ross.troha@teamnorthwoods.com>
2017-03-25 16:04:30 -0400

    added properties and relationships to Artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,10 @@
 class Artist < ApplicationRecord
+  has_many :tracks
+  belongs_to :library
+
+  validates :created_at, :updated_at, :presence => true
+
+  property :name, type: String
+  property :created_at, type: DateTime
+  property :updated_at, type: DateTime
 end
