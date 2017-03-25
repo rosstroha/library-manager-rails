682e2b38531229ff461535b11d289fba7ee904fa
Troha <ross.troha@teamnorthwoods.com>
2017-03-25 16:12:38 -0400

    Playlist model is configured

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,2 +1,12 @@
 class Playlist < ApplicationRecord
+  belongs_to :library
+  has_many :tracks
+  has_many :artists
+
+  validates :created_at, :updated_at, :presence => true
+
+  property :name, type: String
+  property :created_at, type: DateTime
+  property :updated_at, type: DateTime
+  property :libraries_id, type: Integer
 end
