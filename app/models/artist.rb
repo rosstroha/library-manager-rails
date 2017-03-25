3067882dee93b6953ba7847a31b6e85b58b10e26
Troha <ross.troha@teamnorthwoods.com>
2017-03-25 16:09:41 -0400

    added more properties to Artists and updated the model for Library

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,13 @@
 class Artist < ApplicationRecord
   has_many :tracks
   belongs_to :library
+  belongs_to :playlist
 
   validates :created_at, :updated_at, :presence => true
 
   property :name, type: String
   property :created_at, type: DateTime
   property :updated_at, type: DateTime
+  property :libraries_id, type: Integer
+  property :playlists_id, type: Integer
 end
--- app/models/library.rb
+++ app/models/library.rb
@@ -1,2 +1,10 @@
 class Library < ApplicationRecord
+  belongs_to :user
+
+  validates :created_at, :updated_at, :presence => true
+
+  property :name, type: String
+  property :created_at, type: DateTime
+  property :updated_at, type: DateTime
+  property :users_id, type: Integer
 end
