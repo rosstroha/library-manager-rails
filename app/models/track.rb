fc294467ae13d4dac19171f6334ec15a3084d48e
Troha <ross.troha@teamnorthwoods.com>
2017-03-25 16:00:07 -0400

    added relationships to track

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,6 +1,10 @@
 class Track < ApplicationRecord
   validates :title, :import_date, :file_size, :file_name, :full_file_location, :created_at, :updated_at, :presence => true
 
+  belongs_to :artist
+  belongs_to :library
+  belongs_to :playlist
+
   property :title, type: String
   property :import_date, type: Date
 
@@ -22,18 +26,5 @@ class Track < ApplicationRecord
   property :playlist_id, type: Integer
   property :artist_id, type: Integer
   property :libraries_id, type; Integer
-
-  # create_table "tracks", force: :cascade do |t|
-  #   t.string   "title",              null: false
-  #   t.date     "import_date",        null: false
-  #   t.integer  "file_size",          null: false
-  #   t.text     "file_name",          null: false
-  #   t.text     "full_file_location", null: false
-  #   t.datetime "created_at",         null: false
-  #   t.datetime "updated_at",         null: false
-  #   t.index ["artists_id"], name: "index_tracks_on_artists_id", using: :btree
-  #   t.index ["libraries_id"], name: "index_tracks_on_libraries_id", using: :btree
-  #   t.index ["playlists_id"], name: "index_tracks_on_playlists_id", using: :btree
-  # end
 
 end
